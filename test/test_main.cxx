a4ba78bd857235904d6104a05c4e8f1ea550c2ca
Alireza Chegini <ali.chegini.1990@gmail.com>
2022-05-21 12:03:00 +0430

    test/test_main.cxx: Add test code

---
+++ test/test_main.cxx
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "Captcha.h"
+
+
+void test1(){
+  std::vector<unsigned char> buf{};
+  std::string cap {"empty"};
+
+  try{
+
+  int t  = generateCaptcha(buf, cap);
+  
+  if(t !=0){
+    
+    throw std::runtime_error("Error");
+  }
+  cv::Mat m = cv::imdecode(buf , cv::IMREAD_UNCHANGED);
+  cv::imwrite("test1.png",m); 
+ 
+  }catch(const std::exception &ex){
+    std::cout << ex.what() << std::endl;
+  }
+
+}
+
+
+int main(){
+
+  test1();
+}
