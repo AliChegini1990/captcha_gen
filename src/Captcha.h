ccd355429d084574299091f10463482d75fdef87
Alireza Chegini <ali.chegini.1990@gmail.com>
2022-05-21 12:01:55 +0430

    src/Captcha: Add Captcha source file

---
+++ src/Captcha.h
@@ -0,0 +1,21 @@
+#ifndef _CAPTCAH_GEN_
+#define _CAPTCAH_GEN_
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#
+#include <iostream>
+#include <string>
+#include <vector>
+
+#define IMAGE_H 100
+#define IMAGE_W 200
+#define CAPTCHA_LEN 5
+
+
+int
+generateCaptcha(std::vector<unsigned char>& buffer, std::string& captcha_text);
+
+#endif
